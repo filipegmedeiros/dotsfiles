4b5b958896fe03e018fd19f2d9ac2383e863c5d8
filipegmedeiros <filipemedeiros@tuta.io>
2018-03-02 19:25:54 -0300

    Create janelas.sh

---
+++ rofi/.config/rofi/scripts/janelas.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+tema="~/.local/share/rofi/themes/lateral.rasi"
+
+rofi -sidebar-mode -lines 0 -theme "$tema" -show window -bw 4 -eh 1 -opacity "80" -lines 8 -line-margin 4 -width 20 -padding 16
+
